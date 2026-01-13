0e740478c52628f9b6aa4a0030dfa2137288fe07
xafizzafarov44-sudo <xafizzafarov44@gmail.com>
2026-01-13 16:38:05 +0500

    News xafiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
+import News from './Components/News/News';
 function App() {
 
   return (
     <>
         <Header/>
+        <News/>
         <Footer/> 
     </>
   )
