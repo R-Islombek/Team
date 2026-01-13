dcab779d3b8fe2244b5e3ab162df40294e6a16de
H - Komila <komilaxibziddinova06@gmail.com>
2026-01-13 16:58:25 +0500

    Article yangilandi

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Article = () => {
   return (
-    <div>ARTICLE</div>
+    <>
+    
+        <div>
+            <div className='container max-w-[1200px] mx-auto px-4'>
+                <div>
+                    <h1 className='text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1>
+                    <ul>
+                        <li>
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    
+    </>
   )
 }
 
