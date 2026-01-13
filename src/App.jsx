736e63d8e4534dccc4507621dfb66dc0932ce3a7
Your Name <you@example.com>
2026-01-13 15:56:12 +0500

    about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import About from './pages/About/About'
 import Footer from './Components/Footer/Footer';
+
 function App() {
 
   return (
     <>
         <Header/>
+        <About/>
         <Footer/> 
+
     </>
   )
 }
