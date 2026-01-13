a29c7e4f76abca69a5beb0ab316fcf0b953ad847
sevara128@icloud.com <sevara128@icloud.com>
2026-01-13 17:21:29 +0300

    header qvoman

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,23 +2,7 @@ import React from 'react'
 import "./Header.css"
 const Header = () => {
   return (
-    <div>
-          <h1>Hello world</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, eligendi soluta! Pariatur.</p>
-          
-          <h1>Hello world</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, eligendi soluta! Pariatur.</p>
-          
-          <h1>Hello world</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, eligendi soluta! Pariatur.</p>
-          
-          <h1>Hello world</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, eligendi soluta! Pariatur.</p>
-          
-          <h1>Hello world</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, eligendi soluta! Pariatur.</p>
-          
-    </div>
+  <div>Salom</div>
   )
 }
 
