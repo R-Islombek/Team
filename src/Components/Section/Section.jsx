b47de886eb3a8e5a22bbb56ae25900587e1c2798
H - Komila <komilaxibziddinova06@gmail.com>
2026-01-15 02:34:31 +0500

    push tekshirdim

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -1,23 +1,75 @@
-import React from 'react'
+import React from "react";
+import Slider from "react-slick";
+import "./Section.css";
+import Oo from "./images/oo.png";
+import Hh from "./images/hh.png";
+import Uu from "./images/uu.png";
+import Mm from "./images/mm.png";
+import Aa from "./images/aa.png";
 
 const Section = () => {
-  return (
-    <>
-    
-    <div className='section'>
-        <div className='container'>
-            <div className='section__container'>
-                <ul className='section__list'>
-                    <li className='section__item'>
-                        
-                    </li>
-                </ul>
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        cssEase: "linear",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    };
+
+    return (
+        <div className="section">
+            <div className="container">
+                <span>
+                    <h1 className="section__title">Our Recent Projects</h1>
+                    <p className="section__text"> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </span>
+
+                <div className="section__container">
+                    <Slider {...settings}>
+                        <div className="section__item">
+                            <img className="section__img" src={Oo} alt="" />
+                        </div>
+
+                        <div className="section__item">
+                            <img className="section__img" src={Uu} alt="" />
+                        </div>
+
+                        <div className="section__item">
+                            <img className="section__img" src={Hh} alt="" />
+                        </div>
+
+
+                        <div className="section__item">
+                            <img className="section__img" src={Mm} alt="" />
+                        </div>
+
+                        <div className="section__item">
+                            <img className="section__img" src={Aa} alt="" />
+                        </div>
+                    </Slider>
+
+                    <a className="section__btn" href="#">SEE ALL</a>
+                </div>
             </div>
         </div>
-    </div>
-    
-    </>
-  )
-}
+    );
+};
 
-export default Section
+export default Section;
