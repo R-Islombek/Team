587cee30fd0d357ea14649ee247b7133559b86e0
Sizning GitHub username <sizning_email@github.com>
2026-01-14 19:34:34 +0500

    About1

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
+import Jang from './assets/jang.svg'
+import Jang2 from './assets/Jang2.png'
+import './About.css'
 
 const About = () => {
   return (
-    <>
-    <div className='about'>
-      <div className='content'>
-        <div className='about-container'>
-          <h1 className='about-title'>About Us</h1>
-          <p className='about-text'>We are a team of passionate individuals...</p>
+
+    <div className="about">
+      <div className="content">
+        <div className="about-container">
+          <ul>
+            <li className="text">
+              <a href="#" className="about__link">Home </a><span className='about__span'> About us</span>
+              <h1>Lorem Ipsum is simply dummy text of the printing and.</h1>
+
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+              </p>
+
+              <button className="btn">Get in touch →</button>
+            </li>
+
+            <li className="image">
+              <img
+                src={images[current]}
+                alt="About illustration"
+                className="float-img"
+              />
+            </li>
+          </ul>
         </div>
       </div>
     </div>
-    </>
+    
   )
 }
 
